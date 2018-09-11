405a4a460924d59aeaebbcf5f0bdf68d9ac4576e
Randy Herasme <randyherasme6@gmail.com>
2018-09-11 18:03:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if (array.length==2)
+  two_item_str=array.join(" and ")
+  elsif (array.length>=3)
+  final_item=array.pop
+  three_item_str=array.join(", ")
+  three_item_str+", and #{final_item}"
+  else
+  array.join
+  end
 end
